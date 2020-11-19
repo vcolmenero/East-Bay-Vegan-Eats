955bdeb8a53614f45bebd51103c2184ed010f2a2
vcolmenero90 <70417758+vcolmenero90@users.noreply.github.com>
2020-11-19 13:43:26 -0800

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,20 @@
+//mongodb+srv://admin:1234@cluster0.owbib.mongodb.net/East-Bay_Vegan_Eats?retryWrites=true&w=majority
+
+//require modules
+const mongoose = require('mongoose')
+
+//create the shortcut variable
+const db = mongoose.connection
+
+
+//connect to the database
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true, 
+    useUnifiedTopology: true 
+})
+
+//listen for the connection
+db.on('connected', function(){
+    console.log(`connected to MongoDB on ${db.host}:${db.port}`)
+})
